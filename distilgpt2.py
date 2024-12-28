9bc1814178608c005d588dda88dfd9702c7d008f
Jonathan Vongchak <nexusbrainjr@gmail.com>
2024-12-28 02:12:11 +0100

    Initial Commit

---
+++ distilgpt2.py
@@ -0,0 +1,21 @@
+import gradio as gr
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name = 'distilgpt2'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=True)
+
+def distilgpt2_generate_text(text):
+    input = tokenizer.encode(text, return_tensors="pt")
+    output = model.generate(input, max_length= 10, do_sample=True, pad_token_id = tokenizer.eos_token_id)
+    return tokenizer.decode(output[0], skip_special_tokens=True)
+
+
+iface = gr.Interface(
+    fn=distilgpt2_generate_text,
+    inputs='text', 
+    outputs='text',
+    title="Text generation (KendraGPT)",
+    description="Genarate text using my pretrained model. Type a prompt and see how the model generates text"
+)
+iface.launch()
