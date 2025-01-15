d62aee37ae679dd2c6d77424afd482415a9341b7
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-01-15 04:33:37 +0100

    update: increased amount of characters generated

--- distilgpt2.py
+++ distilgpt2.py
@@ -7,7 +7,7 @@ model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=Tr
 
 def distilgpt2_generate_text(text):
     input = tokenizer.encode(text, return_tensors="pt")
-    output = model.generate(input, max_length= 10, do_sample=True, pad_token_id = tokenizer.eos_token_id)
+    output = model.generate(input, max_length= 500, do_sample=True, pad_token_id = tokenizer.eos_token_id)
     return tokenizer.decode(output[0], skip_special_tokens=True)
 
 
--- general_application_model.py
+++ general_application_model.py
@@ -7,7 +7,6 @@ from transformers import (
     TrainingArguments,
     Trainer,
 )
-
 # Function to load and Tokenize dataset
 def prepare_dataset():
     dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
@@ -18,40 +17,36 @@ def prepare_dataset():
     
     # Tokenize the dataset
     tokenized_dataset = dataset.map(
-        lambda x: tokenizer(x["Text"], truncation=True, padding=True), batched=True
+        lambda x: tokenizer(x["text"], truncation=True, padding=True), batched=True
     )
     return tokenized_dataset, tokenizer
-
 # Prepare the model
 def load_model_and_config(tokenizer):
     model = GPT2LMHeadModel.from_pretrained("gpt2")
     model.resize_token_embeddings(len(tokenizer))
     return model
-
 #Trianing Arguments
 def prepare_training_args():
     training_args = TrainingArguments(
-        output_dir="./results",         #Path to save model and logs
-        overwrite_output_dir=True,       #Overwrite previous runs
-        num_train_epochs=3,              #Number of training epochs
-        per_device_train_batch_size=4,   #Training batch size per GPU/CPU
+        output_dir="./results",             #Path to save model and logs
+        overwrite_output_dir=True,          #Overwrite previous runs
+        num_train_epochs=3,                 #Number of training epochs
+        per_device_train_batch_size=4,      #Training batch size per GPU/CPU
         per_device_eval_batch_size=8,       #Evaluation batch size
         warmup_steps=500,                   #Warming stes for learning rate scheduler
-        weight_decay=0.01,                   #Regularization to avoid overfitting
-        logging_dir="./logs",                #Directory for logging
-        save_steps=10_000,                   #Save checkpoint every 10k step
-        save_total_limit=2,                  #Keep only 2 most recent checkpoints
+        weight_decay=0.01,                  #Regularization to avoid overfitting
+        logging_dir="./logs",               #Directory for logging
+        save_steps=10_000,                  #Save checkpoint every 10k step
+        save_total_limit=2,                 #Keep only 2 most recent checkpoints
         
     )
     return training_args
 
 def train_model():
     tokenized_dataset, tokenizer = prepare_dataset()
-    
     #Train and validate dataset
     train_dataset = tokenized_dataset["train"]
     val_dataset = tokenized_dataset["validation"]
-    
     # Loadng the model
     model = load_model_and_config(tokenizer)
     #Prepare the datacollector
@@ -68,10 +63,8 @@ def train_model():
         eval_dataset = val_dataset,
         data_collator=data_collator,
     )
-    
     #Train the model
     trainer.train()
-    
     #saving the model and tokenizer
     trainer.save_model("./trained_model")
     tokenizer.save_pretrained("./trained_model")
